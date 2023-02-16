77bc93fcb63ffed5cb6747237fc1d027a9e28084
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-15 19:42:19 -1000

    setup next / tailwind

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../styles/globals.css'
+
+export default function Home() {
+  return (
+    <div className={"bg-green-200 text-center pt-10 h-screen text-2xl"}>faunadex</div>
+  )
+}
